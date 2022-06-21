641f00f0242833d23279bcc36cff18c8a26b5b46
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-06-21 18:23:32 +0100

    corrected js syntax

--- data/db.js
+++ data/db.js
@@ -12,35 +12,35 @@ export const snippets = [
 
     
     {
-        "title" : "For Loop",
-        "description" : "Use a for loop run the same code over and over again, each time with a different value.",
-        "video" : "https://www.youtube.com/watch?v=Y8sMnRQYr3c",
-        "codesyntax" : [ "for (let i = 0; i <  ; i++) { XX }" ],
-        "docs" : "https://www.w3schools.com/js/js_loop_for.asp"
+        title : "For Loop",
+        description : "Use a for loop run the same code over and over again, each time with a different value.",
+        video : "https://www.youtube.com/watch?v=Y8sMnRQYr3c",
+        codesyntax : [ "for (let i = 0; i <  ; i++) { XX }" ],
+        docs : "https://www.w3schools.com/js/js_loop_for.asp"
     },
     
     {
-        "title" : "Use State",
-        "description" : "The React useState Hook allows us to track state in a function component. State generally refers to data or properties that need to be tracking in an application.",
-        "video" : "https://www.youtube.com/watch?v=4pO-HcG2igk&t=309s",
-        "codesyntax" : [ "const [variable , setVariable] = useState(0);" ],
-        "docs" : "https://www.w3schools.com/react/react_usestate.asp"
+        title : "Use State",
+        description : "The React useState Hook allows us to track state in a function component. State generally refers to data or properties that need to be tracking in an application.",
+        video : "https://www.youtube.com/watch?v=4pO-HcG2igk&t=309s",
+        codesyntax : [ "const [variable , setVariable] = useState(0);" ],
+        docs : "https://www.w3schools.com/react/react_usestate.asp"
     },
 
     {
-        "title" : "While Loop",
-        "description" : "Loops can execute a block of code as long as a specified condition is true. The while loop loops through a block of code as long as a specified condition is true. Watch out for endless loops! ",
-        "video" : "https://www.youtube.com/watch?v=PpbFyLTtpWI",
-        "codesyntax" : [ "while (condition) { // code block to be executed}" ],
-            "docs" : "https://www.w3schools.com/js/js_loop_while.asp"
+        title : "While Loop",
+        description : "Loops can execute a block of code as long as a specified condition is true. The while loop loops through a block of code as long as a specified condition is true. Watch out for endless loops! ",
+        video : "https://www.youtube.com/watch?v=PpbFyLTtpWI",
+        codesyntax : [ "while (condition) { // code block to be executed}" ],
+        docs : "https://www.w3schools.com/js/js_loop_while.asp"
     },
 
     {
-        "title": "Scroll to Top Feature",
-        "description": "Add function to button/event listener which will scroll to top once clicked",
-        "video": "https://www.youtube.com/watch?v=3Jbb3sQVVq0",
-        "codesytax": [ "const scrollTop = () => window.scrollTo(0, 0); scrollTop()" ],
-        "docs": "https://www.w3schools.com/howto/howto_js_scroll_to_top.asp"
+        title : "Scroll to Top Feature",
+        description : "Add function to button/event listener which will scroll to top once clicked",
+        video : "https://www.youtube.com/watch?v=3Jbb3sQVVq0",
+        codesytax : [ "const scrollTop = () => window.scrollTo(0, 0); scrollTop()" ],
+        docs : "https://www.w3schools.com/howto/howto_js_scroll_to_top.asp"
         },
        
     
