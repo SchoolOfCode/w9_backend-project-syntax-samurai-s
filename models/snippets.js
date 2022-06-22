5e4819550a9ecde38235f80c5825a7784b3a3871
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-06-22 18:56:25 +0100

    final commit of wednesday

--- models/snippets.js
+++ models/snippets.js
@@ -2,6 +2,7 @@ import { pool } from "../db/index.js";
 
 export async function getSnippets(searchQuery) {
     console.log(`IT'S HERE! (from models) ${searchQuery} !`);
+        // SQL: select everything from the snippets table where there title contains value1 [expressed here]. || represents a space.
         const result = await pool.query(`SELECT * FROM snippets WHERE title LIKE '%' || $1 || '%'; ` , [searchQuery]);
             console.log(result.rows);
     return result.rows;
