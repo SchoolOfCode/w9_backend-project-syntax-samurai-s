43afdb8e4b4ee4f439f385db9a95aa5f04c40a50
Katie Walters <katie_walters@live.co.uk>
2022-06-23 15:28:09 +0100

    SQL amended

--- models/snippets.js
+++ models/snippets.js
@@ -4,7 +4,7 @@ export async function getSnippets(searchQuery) {
 	console.log(`IT'S HERE! (from models) ${searchQuery} !`);
 	// SQL: select everything from the snippets table where there title contains value1 [expressed here]. || represents a space.
 	const result = await pool.query(
-		`SELECT * FROM snippets WHERE title LIKE '%' || $1 || '%'; `,
+		`SELECT * FROM snippets WHERE LOWER(title) LIKE LOWER(‘%’ || $1 || ‘%’)';`,
 		[searchQuery]
 	);
 	console.log(result.rows);
