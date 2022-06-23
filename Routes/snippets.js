d5212136315dd05deccf75d66ad4a3d7792eb6ac
CJ Chapman <esvagaming@gmail.com>
2022-06-23 10:56:26 +0100

    post functionality working, currently working on delete functionality

--- Routes/snippets.js
+++ Routes/snippets.js
@@ -1,30 +1,32 @@
 import express from 'express';
-import { getSnippets } from '../models/snippets.js';
+import { getSnippets, createNewSnippet } from '../models/snippets.js';
 
 const router = express.Router();
 
-router.get("/" , async function(req , res){
-    const searchQuery = req.query.title;
-    console.log(`searched query log: ${searchQuery}`);
-    // const result = await getSnippets(title);
-    
-   // define the object it'll return
-     const responseObject = {
-         success : true,
-         data : await getSnippets(searchQuery),
-         message : "please heckin work!",
-     }
-     // confirm in the console what the client requested
-     console.log(`router get request worked`)
- 
-     // return the response object (defined above) as JSON
-     res.json(responseObject);
- });
+router.get('/', async function (req, res) {
+	const searchQuery = req.query.title;
+	console.log(`searched query log: ${searchQuery}`);
+	// const result = await getSnippets(title);
 
+	// define the object it'll return
+	const responseObject = {
+		success: true,
+		data: await getSnippets(searchQuery),
+		message: 'please heckin work!',
+	};
+	// confirm in the console what the client requested
+	console.log(`router get request worked`);
 
+	// return the response object (defined above) as JSON
+	res.json(responseObject);
+});
 
+router.post('/', async function (req, res) {
+	const newSnippet = req.body;
+	const result = createNewSnippet(newSnippet);
+	res.json({ success: true, data: await result });
+});
 
- export default router;
+export default router;
 
-
- // NOTE: changed "payload" to "data" because of a frontend / react specific method
+// NOTE: changed "payload" to "data" because of a frontend / react specific method
--- models/snippets.js
+++ models/snippets.js
@@ -1,10 +1,27 @@
-import { pool } from "../db/index.js";
+import { pool } from '../db/index.js';
 
 export async function getSnippets(searchQuery) {
-    console.log(`IT'S HERE! (from models) ${searchQuery} !`);
-        // SQL: select everything from the snippets table where there title contains value1 [expressed here]. || represents a space.
-        const result = await pool.query(`SELECT * FROM snippets WHERE title LIKE '%' || $1 || '%'; ` , [searchQuery]);
-            console.log(result.rows);
-    return result.rows;
-};
+	console.log(`IT'S HERE! (from models) ${searchQuery} !`);
+	// SQL: select everything from the snippets table where there title contains value1 [expressed here]. || represents a space.
+	const result = await pool.query(
+		`SELECT * FROM snippets WHERE title LIKE '%' || $1 || '%'; `,
+		[searchQuery]
+	);
+	console.log(result.rows);
+	return result.rows;
+}
 
+export async function createNewSnippet(newSnippet) {
+	const result = await pool.query(
+		`INSERT INTO snippets(title, description, video, codesyntax, docs) VALUES ($1, $2, $3, $4, $5) RETURNING *;`,
+		[
+			newSnippet.title,
+			newSnippet.description,
+			newSnippet.video,
+			newSnippet.codesyntax,
+			newSnippet.docs,
+		]
+	);
+
+	return result.rows;
+}
